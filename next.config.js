69990a2d80c4feea131d35940896f0876abb6e37
Dhani Kuswendha <skripc@gmail.com>
2023-03-20 10:49:52 +0700

    update mongo connection

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    //MONGODB_URI: "mongodb://localhost:27017",
+    //MONGODB_URI: "mongodb+srv://vercel-admin-user:lTSiFgl7S927VVl1@cluster0.8vvd8tj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     HOSTNAME: "localhost",
     PORT: 3000,
   }
